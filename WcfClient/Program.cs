162fdd34ec5210f91c9a595729eafb482e66cbe3
Sergola05 <poryadin.serg@yandex.ru>
2025-02-06 17:52:32 +0300

    Добавьте файлы проекта.

---
+++ WcfClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfSubj;
+
+namespace WcfSubj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1 client = new Service1();
+            string result = client.GetMessage();
+            Console.WriteLine(result);
+        }
+    }
+}
